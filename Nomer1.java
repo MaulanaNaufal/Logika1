cee3a9333390f3a904b03e84b5b085a9ea4b10ab
MaulanaNaufal <82005991+MaulanaNaufal@users.noreply.github.com>
2022-04-13 09:22:04 +0700

    Add files via upload

---
+++ Nomer1.java
@@ -0,0 +1,12 @@
+public class Nomer1{
+    
+  public static void main(String[] args){
+    int x = 1;
+    int y = 1000;
+    if(x < y){
+      System.out.println("nilai " + y + " adalah nilai terbesar");
+    } else {
+      System.out.println("nilai " + x + " adalah nilai terbesar");
+    }
+  }
+}
